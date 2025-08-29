5fab16a141992dc86767694ebc4d50b30b978c49
khushichetule09 <khushichetule2@gmail.com>
2025-08-29 23:01:02 +0530

    initial project setup with Skiper and wrap button

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import WrapButton from '@/components/ui/wrap-button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-white'>Hello world!</h1>
+      <WrapButton>Get started</WrapButton>
+    </div>
+  )
+}
+
+export default page
