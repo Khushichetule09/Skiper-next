65168fc064051318b479eba2ee05c577054ec7a6
khushichetule09 <khushichetule2@gmail.com>
2025-09-02 15:05:40 +0530

    Add initial project setup and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import WrapButton from '@/components/ui/wrap-button'
+
+import AiInputCard from '@/components/homeCards/AiInputCard'
+import CardCarouselParent from '@/components/homeCards/CardCarouselParent'
+import SpecialCard from '@/components/homeCards/SpecialCard'
+import TemplateCard from '@/components/homeCards/TemplateCard'
+import ThemeChange from '@/components/homeCards/ThemeChange'
+import HeroContent from '@/components/landingPage/HeroContent'
+import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <h1 className='text-white'>Hello world!</h1>
-      <WrapButton>Get started</WrapButton>
+      <Navbar />
+      <HeroContent />
+      <CardCarouselParent />
+      <TemplateCard />
+      <SpecialCard />
+      <AiInputCard />
+      <ThemeChange />
     </div>
   )
 }
